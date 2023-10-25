0bca97f5b77fbf37aa0a74204cd19eac819245b0
Eliud Duno <eliudduno@outlook.com>
2023-10-24 20:47:27 -0400

    Initial commit

---
+++ Applications/Programar vídeo YouTube Python/source_code.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+
+while break_count < total_breaks:
+    time.sleep(10)
+    webbrowser.open("https://youtu.be/9AB2tVvjDEU?si=H_Yg8Lx9s7lTJ25p")
+    break_count = break_count + 1
