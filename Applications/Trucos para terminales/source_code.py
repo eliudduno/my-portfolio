011d0b9b9896b41ef83a1b478246f883cf11a1ca
Eliud Duno <eliudduno@outlook.com>
2023-10-24 20:13:00 -0400

    initial commit

---
+++ Applications/Trucos para terminales/source_code.py
@@ -0,0 +1,17 @@
+from colorama import init
+init()
+from colorama import Fore , Back , Style # Fore- font color | Back- Background
+
+print(Fore.GREEN,"Saludos a todos!!!")
+
+print(Back.RED,"Saludos a todos!!!")
+
+#para volver al aburrido blanco y negro: print(Styoe.RESET_ALL)
+'''
+All Variaton on Colors: 
+
+Fore: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Back: BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET
+Style: DIM NORMAL BRIGHT RESET_ALL
+  
+'''
